c23eb1b06777ea646991f77725a1a94ed02d4f16
oreki <81974116+oreki-nginamo@users.noreply.github.com>
2024-07-08 23:50:59 +0800

    Add files via upload
    second upload - mejo polished

--- decision.py
+++ decision.py
@@ -5,6 +5,16 @@ import numpy as np
 # commands based on the output of the perception_step() function
 def decision_step(Rover):
 
+    # offset in rad used to hug the left wall.
+    offset = 0
+    # Only apply left wall hugging when out of the starting point (after 10s)
+    # to avoid getting stuck in a circle
+    if Rover.total_time > 10:
+        # Steering proportional to the deviation results in
+        # small offsets on straight lines and
+        # large values in turns and open areas
+        offset = 0.8 * np.std(Rover.nav_angles)
+
     # Check if we have vision data to make decisions with
     if Rover.nav_angles is not None:
         
@@ -41,29 +51,46 @@ def decision_step(Rover):
             # Return early since we are prioritizing rock sample
             return Rover
 
-        # Check for Rover.mode status
-        if Rover.mode == 'forward':
+        # Check if we have vision data to make decisions with
+    if Rover.nav_angles is not None:
+        # Check for Rover.mode status. I made Rover.mode a stack
+        if Rover.mode[-1] == 'forward':
+
             # Check the extent of navigable terrain
-            if len(Rover.nav_angles) >= Rover.stop_forward:  
-                # If mode is forward, navigable terrain looks good 
-                # and velocity is below max, then throttle 
-                if Rover.vel < Rover.max_vel:
+            if len(Rover.nav_angles) >= Rover.stop_forward:
+                # If mode is forward, navigable terrain looks good
+                # Except for start, if stopped means stuck.
+                # Alternates between stuck and forward modes
+                if Rover.vel <= 0.1 and Rover.total_time - Rover.stuck_time > 4:
+                    # Set mode to "stuck" and hit the brakes!
+                    Rover.throttle = 0
+                    # Set brake to stored brake value
+                    Rover.brake = Rover.brake_set
+                    Rover.steer = 0
+                    Rover.mode.append('stuck')
+                    Rover.stuck_time = Rover.total_time
+                # if velocity is below max, then throttle
+                elif Rover.vel < Rover.max_vel:
                     # Set throttle value to throttle setting
                     Rover.throttle = Rover.throttle_set
                 else: # Else coast
                     Rover.throttle = 0
                 Rover.brake = 0
                 # Set steering to average angle clipped to the range +/- 15
-                Rover.steer = np.clip(np.mean(Rover.nav_angles * 180/np.pi), -15, 15)
+                # Rover.steer = np.clip(np.mean(Rover.nav_angles * 180/np.pi), -15, 15)
+                # Hug left wall by setting the steer angle slightly to the left
+                Rover.steer = np.clip(np.mean((Rover.nav_angles+offset) * 180 / np.pi), -15, 15)
+
             # If there's a lack of navigable terrain pixels then go to 'stop' mode
-            elif len(Rover.nav_angles) < Rover.stop_forward:
+            elif len(Rover.nav_angles) < Rover.stop_forward or Rover.vel <= 0:
                     # Set mode to "stop" and hit the brakes!
                     Rover.throttle = 0
                     # Set brake to stored brake value
                     Rover.brake = Rover.brake_set
                     Rover.steer = 0
-                    Rover.mode = 'stop'
-         # If we're already in "stuck". Stay here for 1 sec
+                    Rover.mode.append('stop')
+
+            # If we're already in "stuck". Stay here for 1 sec
         elif Rover.mode[-1] == 'stuck':
             # if 1 sec passed go back to previous mode
             if Rover.total_time - Rover.stuck_time > 1:
@@ -83,8 +110,9 @@ def decision_step(Rover):
                 # Turn range is +/- 15 degrees, when stopped the next line will induce 4-wheel turning
                 # Since hugging left wall steering should be to the right:
                 Rover.steer = -15
+
         # If we're already in "stop" mode then make different decisions
-        elif Rover.mode == 'stop':
+        elif Rover.mode[-1] == 'stop':
             # If we're in stop mode but still moving keep braking
             if Rover.vel > 0.2:
                 Rover.throttle = 0
@@ -98,7 +126,8 @@ def decision_step(Rover):
                     # Release the brake to allow turning
                     Rover.brake = 0
                     # Turn range is +/- 15 degrees, when stopped the next line will induce 4-wheel turning
-                    Rover.steer = -15 # Could be more clever here about which way to turn
+                    # Since hugging left wall steering should be to the right:
+                    Rover.steer = -15
                 # If we're stopped but see sufficient navigable terrain in front then go!
                 if len(Rover.nav_angles) >= Rover.go_forward:
                     # Set throttle back to stored value
@@ -106,13 +135,16 @@ def decision_step(Rover):
                     # Release the brake
                     Rover.brake = 0
                     # Set steer to mean angle
-                    Rover.steer = np.clip(np.mean(Rover.nav_angles * 180/np.pi), -15, 15)
-                    Rover.mode = 'forward'
-                    
+                    # Hug left wall by setting the steer angle slightly to the left
+                    offset = 12
+                    Rover.steer = np.clip(np.mean(Rover.nav_angles * 180 / np.pi) + offset, -15, 15)
+                    Rover.mode.pop()  # returns to previous mode
+              
     # If no vision data is available
     else:
         Rover.throttle = Rover.throttle_set
         Rover.steer = 0
         Rover.brake = 0      
-    
+    if Rover.near_sample and not Rover.picking_up:
+        Rover.send_pickup = True
     return Rover
--- drive_rover.py
+++ drive_rover.py
@@ -40,6 +40,8 @@ class RoverState():
     def __init__(self):
         self.start_time = None # To record the start time of navigation
         self.total_time = None # To record total duration of naviagation
+        self.stuck_time = 0 # To record moment that got stuck
+        self.rock_time = 0 # To record moment that started to go for the near rock sample
         self.img = None # Current camera image
         self.pos = None # Current position (x, y)
         self.yaw = None # Current yaw angle
@@ -49,19 +51,21 @@ class RoverState():
         self.steer = 0 # Current steering angle
         self.throttle = 0 # Current throttle value
         self.brake = 0 # Current brake value
-        self.nav_angles = None # Angles of navigable terrain pixels
+        self.nav_angles = None  # Angles of navigable terrain pixels
         self.nav_dists = None # Distances of navigable terrain pixels
+        self.samples_angles = None  # Angles of rock sample pixels
+        self.samples_dists = None  # Distances of rock sample pixels
         self.ground_truth = ground_truth_3d # Ground truth worldmap
-        self.mode = 'forward' # Current mode (can be forward or stop)
-        self.throttle_set = .7 # Throttle setting when accelerating
+        self.mode = ['forward'] # Current mode (can be forward or stop)
+        self.throttle_set = 0.5 # Throttle setting when accelerating
         self.brake_set = 10 # Brake setting when braking
         # The stop_forward and go_forward fields below represent total count
         # of navigable terrain pixels.  This is a very crude form of knowing
         # when you can keep going and when you should stop.  Feel free to
         # get creative in adding new fields or modifying these!
-        self.stop_forward = 50 # Threshold to initiate stopping
+        self.stop_forward = 100 # Threshold to initiate stopping
         self.go_forward = 500 # Threshold to go forward again
-        self.max_vel = 2 # Maximum velocity (meters/second)
+        self.max_vel = 3 # Maximum velocity (meters/second)
         # Image output from perception step
         # Update this image to display your intermediate analysis steps
         # on screen in autonomous mode
